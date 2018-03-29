2ebfdc8e04f77d72b97b988212c5e9d713c24cc8
RhiobeT <rhiobet@gmail.com>
2018-03-29 16:35:42 +0200

    Added benchmarks for interesting uses of "final" + references

--- JMHBenchmarks/src/main/java/fr/rhiobet/Main.java
+++ JMHBenchmarks/src/main/java/fr/rhiobet/Main.java
@@ -6,15 +6,19 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class Main {
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include("fr.rhiobet.benchmarks.BenchStreamVsListSum")
-                .include("fr.rhiobet.benchmarks.BenchStreamVsListIO")
-                .warmupIterations(5)
-                .measurementIterations(5)
-                .forks(1)
-                .shouldFailOnError(true)
-                .build();
-        new Runner(opt).run();
-    }
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+//        .include("fr.rhiobet.benchmarks.BenchStreamVsListSum")
+//        .include("fr.rhiobet.benchmarks.BenchStreamVsListIO")
+        .include("fr.rhiobet.benchmarks.BenchFinalNormal")
+        .include("fr.rhiobet.benchmarks.BenchFinalArray")
+        .include("fr.rhiobet.benchmarks.BenchFinalAll")
+        .warmupIterations(10)
+        .measurementIterations(20)
+        .forks(1)
+        .shouldFailOnError(true)
+        .build();
+    
+    new Runner(opt).run();
+  }
 }
--- resources/README.md
+++ resources/README.md
@@ -1,6 +1,16 @@
-# Ressources intéressantes
+# Interesting resources
 
-## Performances de l'API Stream
-
+## Stream API performance issues
 * https://jaxenter.com/java-performance-tutorial-how-fast-are-the-java-8-streams-118830.html
 * https://blog.codefx.org/java/stream-performance/
+
+## "final" keyword
+* https://www.ibm.com/developerworks/java/library/j-jtp1029/index.html
+* https://stackoverflow.com/questions/39521587/java-final-performance-optimization#comment66387318_39521679
+
+## JIT optimizations
+* https://blog.takipi.com/java-on-steroids-5-super-useful-jit-optimization-techniques/
+
+## Data dependencies and parallelization
+* https://www.researchgate.net/publication/278705238_Dependency-Based_Automatic_Parallelization_of_Java_Applications
+* http://aeminium.dei.uc.pt/index.php/AEminium
